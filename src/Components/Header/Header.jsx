eae99a274c82bf2ea7136b618ffdc0e7843b6873
aalon <aaron@geekstudio.co.kr>
2024-01-04 22:15:43 +0900

    Header 추가

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,263 @@
+import {
+    HeaderLayout,
+    HeaderContainer,
+    CategoryContainer,
+    Logo,
+    SideButtonContainer,
+    SearchBox,
+    InputBox,
+    RightBtn,
+    RightBtnBox,
+    GnbLayout,
+    GnbContainer,
+    Gnb,
+    GnbFont,
+    RightBtnCont,
+    WriteBtn, CategoryCont, RankLayout, Rankcont, RankTitle, Ranking
+} from './Header.styled';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/scrollbar';
+import "swiper/css/effect-fade";
+import "swiper/css/pagination";
+import SwiperCore, { EffectCoverflow, Autoplay, EffectFade, Navigation, Pagination } from "swiper/modules";
+
+const Header = ({ children, ...props }) => {
+    return (
+        <HeaderLayout>
+            <HeaderContainer>
+                <Logo href="/">
+                    <div>오늘의집</div>
+                </Logo>
+                <CategoryContainer>
+                    <CategoryCont active="true">
+                        <span>커뮤니티</span>
+                    </CategoryCont>
+                    <CategoryCont>
+                        <span>쇼핑</span>
+                    </CategoryCont>
+                    <CategoryCont>
+                        <span>이사/시공/생활</span>
+                    </CategoryCont>
+                </CategoryContainer>
+                <SideButtonContainer>
+                    <SearchBox>
+                        <InputBox padding="7" type="image" src="https://www.esthermall.co.kr/data/skin/front/moment/img/icon/ico_gnb_search.svg"/>
+                        <InputBox search="true" type="text" placeholder="통합검색"/>
+                    </SearchBox>
+                    <RightBtnBox>
+                        <RightBtn>
+                            <RightBtnCont href="/login" none="true">로그인</RightBtnCont>
+                            <RightBtnCont href="/join">회원가입</RightBtnCont>
+                            <RightBtnCont href="/service">고객센터</RightBtnCont>
+                            <WriteBtn>
+                                <span>글쓰기</span>
+                            </WriteBtn>
+                        </RightBtn>
+                    </RightBtnBox>
+                    <div>
+
+                    </div>
+                </SideButtonContainer>
+            </HeaderContainer>
+            <GnbContainer>
+                <GnbLayout>
+                    <Gnb>
+                        <GnbFont href="/login" active="true">
+                            <span>홈</span>
+                        </GnbFont>
+                        <GnbFont href="/login">
+                            <span>추천</span>
+                        </GnbFont>
+                        <GnbFont href="/login">
+                            <span>#채널</span>
+                        </GnbFont>
+                        <GnbFont href="/login">
+                            <span>집들이</span>
+                        </GnbFont>
+                        <GnbFont href="/login">
+                            <span>집사진</span>
+                        </GnbFont>
+                        <GnbFont href="/login">
+                            <span>3D인테리어</span>
+                        </GnbFont>
+                        <GnbFont href="/login">
+                            <span>살림수납</span>
+                        </GnbFont>
+                        <GnbFont href="/login">
+                            <span>콜렉터블</span>
+                        </GnbFont>
+                        <GnbFont href="/login">
+                            <span>홈스토랑</span>
+                        </GnbFont>
+                        <GnbFont href="/login">
+                            <span>핫플레이스</span>
+                        </GnbFont>
+                        <GnbFont href="/login">
+                            <span>육아</span>
+                        </GnbFont>
+                        <GnbFont href="/login">
+                            <span>플랜테리어</span>
+                        </GnbFont>
+                        <GnbFont href="/login">
+                            <span>반려동물</span>
+                        </GnbFont>
+                        <GnbFont href="/login">
+                            <span>캠핑</span>
+                        </GnbFont>
+                        <GnbFont href="/login">
+                            <span>취미</span>
+                        </GnbFont>
+                        <GnbFont href="/login">
+                            <span>이벤트</span>
+                        </GnbFont>
+                    </Gnb>
+                </GnbLayout>
+                <RankLayout className="group">
+                    <Swiper
+                        style={{height: '20px'}}
+                        direction={'vertical'}  // 방향
+                        allowTouchMove={false}  // 스크롤 막기
+                        loop={true} // 반복
+                        autoplay={{ delay: 2000, disableOnInteraction: false }} // 자동재생
+                        pagination={false}
+                        modules={[Autoplay, EffectCoverflow, Pagination]}
+                    >
+                        <SwiperSlide>
+                            <span>1</span>
+                            <span></span>
+                            <span>두닷 책상</span>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <span>2</span>
+                            <span></span>
+                            <span>두루마리휴지걸이</span>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <span>3</span>
+                            <span></span>
+                            <span>공기정화 식물</span>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <span>4</span>
+                            <span></span>
+                            <span>오일병</span>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <span>5</span>
+                            <span></span>
+                            <span>옷걸이스탠드</span>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <span>6</span>
+                            <span></span>
+                            <span>벙커침대책상</span>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <span>7</span>
+                            <span></span>
+                            <span>코렐그릇세트</span>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <span>8</span>
+                            <span></span>
+                            <span>냉장고 자석</span>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <span>9</span>
+                            <span></span>
+                            <span>일룸</span>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <span>10</span>
+                            <span></span>
+                            <span>침대 패드</span>
+                        </SwiperSlide>
+                    </Swiper>
+
+                    <Rankcont >
+                        <RankTitle>
+                            <header className="flex">인기검색어</header>
+                        </RankTitle>
+                        <Ranking>
+                            <a>
+                                <div>
+                                    <span>1</span>
+                                    <span></span>
+                                    <span>두닷 책상</span>
+                                </div>
+                            </a>
+                            <a>
+                                <div>
+                                    <span>2</span>
+                                    <span></span>
+                                    <span>두루마리휴지걸이</span>
+                                </div>
+                            </a>
+                            <a>
+                                <div>
+                                    <span>3</span>
+                                    <span></span>
+                                    <span>공기정화 식물</span>
+                                </div>
+                            </a>
+                            <a>
+                                <div>
+                                    <span>4</span>
+                                    <span></span>
+                                    <span>오일병</span>
+                                </div>
+                            </a>
+                            <a>
+                                <div>
+                                    <span>5</span>
+                                    <span></span>
+                                    <span>옷걸이스탠드</span>
+                                </div>
+                            </a>
+                            <a>
+                                <div>
+                                    <span>6</span>
+                                    <span></span>
+                                    <span>벙커침대책상</span>
+                                </div>
+                            </a>
+                            <a>
+                                <div>
+                                    <span>7</span>
+                                    <span></span>
+                                    <span>코렐그릇세트</span>
+                                </div>
+                            </a>
+                            <a>
+                                <div>
+                                    <span>8</span>
+                                    <span></span>
+                                    <span>냉장고 자석</span>
+                                </div>
+                            </a>
+                            <a>
+                                <div>
+                                    <span>9</span>
+                                    <span></span>
+                                    <span>일룸</span>
+                                </div>
+                            </a>
+                            <a>
+                                <div>
+                                    <span>10</span>
+                                    <span></span>
+                                    <span>침대 패드</span>
+                                </div>
+                            </a>
+                        </Ranking>
+                    </Rankcont>
+
+                </RankLayout>
+            </GnbContainer>
+        </HeaderLayout>
+    );
+}
+
+export default Header;
---
+++ src/Components/Header/Header.styled.js
@@ -0,0 +1,256 @@
+import styled, {css} from 'styled-components';
+import tw from "twin.macro";
+
+export const HeaderLayout = styled.div`
+    max-width: 2545px;
+    border-bottom: 1px solid #EAEDEF;
+`;
+
+export const GnbContainer = styled.div`
+  display: flex;
+  max-width: 1256px;
+  margin: 0 auto;
+  padding: 0 60px;
+  height: inherit;
+  align-items: center;
+  ${tw`
+        overflow-hidden
+    `}
+`;
+
+export const GnbLayout = styled.div`
+  height: 51px;
+
+  span {
+    height: inherit;
+    margin: 0 5px;
+  }
+`;
+
+export const Gnb = styled.nav`
+     display: flex;
+     max-width: 920px;
+  
+    ${tw`
+        overflow-hidden
+        h-full
+    `}
+`;
+
+export const GnbFont = styled.a`
+    ${tw`
+        text-tit-black
+        font-bold
+        text-base
+        hover:text-sky-blue
+        h-full
+        px-6px
+        py-12px
+        w-auto
+        no-underline
+    `}
+    
+    span {
+      ${tw`
+        block
+        whitespace-nowrap
+        h-full
+      `}
+    }
+
+
+`;
+
+export const RankLayout = styled.div`
+  ${tw`
+    h-51px
+    flex
+    justify-center
+    items-center
+  `}
+`;
+
+export const Rankcont = styled.div`
+  ${tw`
+    invisible
+    group-hover:visible
+    shadow-lg
+    rounded-md
+    translate-y-56
+    bg-white
+    absolute
+    w-270px
+    h-480px
+    z-50
+    py-5
+  `}
+`;
+
+export const RankTitle = styled.div`
+  ${tw`
+    mr-4
+    mb-2
+    ml-4
+  `}
+`;
+
+export const Ranking = styled.div`
+    ${tw`
+        w-full
+        h-full
+    `}
+    
+    a {
+      ${tw`
+        flex
+        h-10
+        mx-4
+        my-2
+        items-center
+      `}
+    }
+
+      div {
+        ${tw`
+            h-6
+            w-full
+          `}
+      }
+  
+    span {
+      &&:first-child {
+        width: 20px;
+        height: 20px;  
+      }
+    }
+`;
+
+export const HeaderContainer = styled.div`
+    max-width: 1256px;
+    margin: 0 auto;
+    padding: 10px 50px;
+    height: 80px;
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const CategoryContainer = styled.div`
+    width: 378px;
+    display: flex;
+    -webkit-flex: 1 1;
+    -ms-flex: 1 1;
+    flex: 1 1;
+`;
+
+export const CategoryCont = styled.a`
+      padding: 21px 5px 15px;
+      margin: 0 10px;
+      font-weight: bold;
+      font-size: 18px;
+      ${tw`text-tit-black hover:text-sky-blue cursor-pointer`}
+      ${props =>
+    props.active &&
+    tw`
+          text-sky-blue
+          `
+}
+      span {
+        padding: 4px;
+        margin: -4px;
+        font-size: 18px;
+        font-weight: bold;
+      }
+`;
+
+export const SideButtonContainer = styled.div`
+    display: flex;
+    flex: 0 1 650px;
+`;
+
+export const SearchBox = styled.div`
+    border: 1px solid #DADDE0;
+    width: 296px;
+    height: 38px;
+    border-radius: 4px;
+    padding: 0px 10px;
+    display: flex;
+`;
+
+export const InputBox = styled.input`
+  padding: ${props => props.padding || 0}px;
+  ${props =>
+    props.search &&
+    css`
+        width: 100%;
+        outline: none;
+        border: none;
+        font-size: 16px;
+        line-height: 24px;
+  `};
+`;
+
+export const RightBtnBox = styled.div`
+  position: relative;
+  width: 310px;
+`;
+
+export const RightBtn = styled.div`
+  width: 100%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const RightBtnCont = styled.a`
+  ${tw`
+    text-sm
+    px-3
+    border-l
+    text-tit-black
+    no-underline
+  `}
+  
+  ${props =>
+    props.none &&
+    tw`
+      border-none
+      `
+}
+`;
+
+export const WriteBtn = styled.button`
+  ${tw`
+    inline-flex
+    items-center
+    rounded-md
+    w-24
+    h-10
+    bg-sky-blue
+    hover:bg-sky-blue-hover
+  `}
+  
+  & {
+    span {
+      ${tw`inline-flex rounded-md border-none items-center text-sm w-full text-white px-4 h-10`}
+    }
+  }
+`;
+
+export const Logo = styled.a`
+  ${tw`
+    text-2xl
+    text-tit-black
+    font-bold
+    w-112px
+    h-30px
+    cursor-pointer
+    no-underline
+  `}
+  
+    div {
+      width: 100%;
+      height: 100%;
+    }
+`;
